32f4659a051daedb2bca5801b49419f39bf5a2ef
demasharaf <dema.sharaf1@gmail.com>
2025-07-23 13:31:02 +0200

    fix fav bug

--- backend/routes/favorite.js
+++ backend/routes/favorite.js
@@ -4,7 +4,7 @@ const favoriteController = require('../controllers/favoriteController');
 const { authMiddleware } = require('../middleware/auth');
 
 router.post('/', authMiddleware, favoriteController.addToFavorites);
-router.delete('/:car_id', authMiddleware, favoriteController.removeFromFavorites);
+router.delete('/:carId', authMiddleware, favoriteController.removeFromFavorites);
 router.get('/', authMiddleware, favoriteController.getFavoriteCars);
 
 module.exports = router;
